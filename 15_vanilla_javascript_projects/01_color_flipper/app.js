d0621bc93b83829e70f759327e6c265f25ca5304
MariusDevelops <brazasmarius@gmail.com>
2021-10-13 02:24:54 +0200

    Color flipper project analysis

--- 15_vanilla_javascript_projects/01_color_flipper/app.js
+++ 15_vanilla_javascript_projects/01_color_flipper/app.js
@@ -7,18 +7,17 @@ btn.addEventListener('click', function(){ // what happens then you press button
     // console.log(document.body) checks that he have body in console
 
 // get random number between 0 - 3 (0 = green)
-    const randomNumber = getRandomNumber(); // creates new return variable with new function
+    const randomNumber = getRandomNumber(); // creates new variable with new function, basicly... value for colors variable
     // console.log(randomNumber);
     // creates random number, but it can be specific for ex.: =2
     // getRandomNumber() is s funcion written below
 
-    document.body.style.backgroundColor = colors[randomNumber];
-    // targets document, body, then looks for style, backgroundColor
-    // to colors array and variable randomNumber
-    // so HTML background = color in array. Now its random. But it can be 0, 1, 2 or 3
-    color.textContent = colors[randomNumber];
-    // selects color to display what is bacground color
-    // selects class and changes its text to colors array value
+    document.body.style.backgroundColor = colors[randomNumber]; // colors[2]
+    // targets document, body, then looks for style to change backgroundColor to random color
+    // so HTML background = color in array. Now its random. But it can be 0, 1, 2 or 3 or just color name.
+
+    color.textContent = colors[randomNumber]; // colors[2]
+    // selects html class .color and changes its text to random colors array value
 })
 
 function getRandomNumber() {
@@ -28,17 +27,20 @@ function getRandomNumber() {
     // but we need round up number so we use Math.floor, rounds up to closest intinger 0.9 rounds up to 0
 }
 
-/* recap:
+/*
+    Recap:
+
+    Objective: Once you press button, background and text changes to random color.
 
     1. Define variables.
-    1.1. Colors you will use for background.
-    1.2. Target button that will need get activated.
-    1.3. Target color span you use to show text color.
+    1.1. Colors you will use for a background.
+    1.2. Target button that gets activated.
+    1.3. Target text that shows background color.
 
     2. Function what happens when you press button.
-    2.1. Function returns new variable with one more random number function.
-    2.2. Function changes background color to random colors number.
-    2.3. Function changes text to random colors number.
+    2.1. Create variable with random number.
+    2.2. Propert that changes background to random color.
+    2.3. Property that changes text to random color name.
 
-    3. One more function to get random number. Used for 2.1., variable.
+    3. Function to get random number. Used for 2.1., variable.
 */
