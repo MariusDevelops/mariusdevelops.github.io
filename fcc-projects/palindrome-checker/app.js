b10b033f0d2fbfdbfd47ad763eb3a06c3c5d24e2
MariusDevelops <brazasmarius@gmail.com>
2021-10-13 22:52:41 +0200

    Counter project analysis

--- fcc-projects/palindrome-checker/app.js
+++ fcc-projects/palindrome-checker/app.js
@@ -1,23 +1,18 @@
-var myForm = document.getElementById('myForm');
-var wordInput = document.getElementById('wordInput');
-var answer = document.getElementById('answer');
+// declare variables
+// The getElementById() method returns the element that has the ID attribute with the specified value.
+var button = document.getElementById("button");
+var result = documetn.getElementById("result");
 
-myForm.addEventListener('submit', function(e){
-  e.preventDefault();
- 
-  var userInput = wordInput.value;
-    
-  answer.innerHTML = isPalindrome(userInput);
-  
+// The addEventListener() method attaches an event handler to the specified element.
+// button.addEventListener('click', function(){ })
+button.addEventListener("click", () => { // arrow function syntac also valiable choice
+
+    result.innerHTML = "Loading... Please wait";
+
+    var timeout = setTimeout(() => {
+        var input = document.getElementById("userInput").value;
+        // test it in console
+        console.long(input)
+
+    },900) // time in milli seconds. 0.9 seconds.
 });
-
-function isPalindrome(userInput){
-  
-  for ( i = 0; i <= ( userInput.length / 2 ); i++ ) {
-    if ( userInput[i] != userInput[(userInput.length - 1 - i)] ) {
-      return false;
-    } 
-  }
-  return true;
-}
-
