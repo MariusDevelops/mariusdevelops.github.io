a9b0b355cdba32e81d96ae2c6d00d74a1015249e
MariusDevelops <brazasmarius@gmail.com>
2021-10-16 23:48:53 +0200

    Sidebar project analysis

--- 15_vanilla_javascript_projects/05_sidebar/app.js
+++ 15_vanilla_javascript_projects/05_sidebar/app.js
@@ -8,9 +8,27 @@ toggleBtn.addEventListener("click", function () {
   // } else {
   //   sidebar.classList.add("show-sidebar");
   // }
-  sidebar.classList.toggle("show-sidebar");
+  sidebar.classList.toggle("show-sidebar"); // toogle is a shourtcut for if/else statement
 });
 
 closeBtn.addEventListener("click", function () {
   sidebar.classList.remove("show-sidebar");
 });
+
+/*
+    Recap:
+
+    Objective: once you press nav icon it shows list, and once you press it again it removes list.
+    Once you press X button sidebar hides.
+
+    1. Define variables.
+    1.1. Target sidebar-toggle button.
+    1.2. Target close button.
+    1.3. Target all sidebar content.
+
+    2. Create a function to show or hide list.
+    2.1. One option is to use if/else statements to show or hide links.
+    2.1. Another option is to use toggle method.
+    
+    3. Create one more function to hide sidebar by pressing close button.
+*/
