97b978b879030c5e9239e96db74f31e37289d47a
MariusDevelops <brazasmarius@gmail.com>
2021-10-17 23:42:22 +0200

    Modal project analysis

--- 15_vanilla_javascript_projects/06_modal/app.js
+++ 15_vanilla_javascript_projects/06_modal/app.js
@@ -13,3 +13,18 @@ modalBtn.addEventListener("click", function () {
 closeBtn.addEventListener("click", function () {
   modal.classList.remove("open-modal");
 });
+
+/*
+    Recap:
+
+    Objective: once you press nav icon it shows modal, and once you press x button it removes modal.
+
+    1. Define variables.
+    1.1. Target modal button.
+    1.2. Target close button.
+    1.3. Target modal overlay.
+
+    2. Create a function to functions show and hide modal.
+    2.1. Target modal, use method to show classes and add open-modal class.
+    2.1. Target modal, use method to show classes and remove open-modal class.
+*/
